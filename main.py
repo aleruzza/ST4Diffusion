4ef27da800c7966f5c743b558647a9e69eaff59d
Alessandro Ruzza <aleruzza@pop-os.localdomain>
2024-04-30 17:35:11 -0400

    added missing definition of n_sample

--- main.py
+++ main.py
@@ -173,6 +173,7 @@ def train_eor():
     # parameters for sampling
     sample_freq = params['sample_freq'] # the period of sampling
     test_paradf = pd.read_csv(f'data/testpara.csv', index_col=0)
+    n_sample = len(test_paradf)
     test_param = torch.tensor(np.array(test_paradf[['PlanetMass', 'AspectRatio']]))
     test_param =  test_param.to(device)
     
