a94d9ed4798c967cf5f500a20faf06b3767bb4d7
Alessandro Ruzza <aleruzza@pop-os.localdomain>
2024-05-06 11:36:30 -0400

    loader.py

--- loader.py
+++ loader.py
@@ -17,7 +17,75 @@ def get_labels_narray(base_path):
     labels = np.log10(labels)
     return labels
 
+def get_pretraining_data(base_path):
+    dataset = np.load(f'{base_path}/swe_data.npy')
+    dataset = dataset.reshape(-1, 128, 128)
+    #np.random.shuffle(dataset)
+    return dataset
 
+
+
+
+class PretrainDataset(Dataset):
+    """Dataset for pretraining
+
+ 
+    """
+ 
+    def __init__(
+        self,
+        folder="",
+        image_size=128,
+        shuffle=False,
+        n_param =6,
+    ):
+        """Init
+
+        Parameters
+        ----------
+        folder : str, optional
+            folder where the .npy files are stored, by default ""
+        image_size : int, optional
+            pixel dimension of the images in the dataset, by default 128
+        shuffle : bool, optional
+            enables shuffling of the dataset, by default False
+        n_param : int, optional
+            number of conditional parameters for which the model is built.
+            Note that during pretraining all these parameters
+            will be set to 0, by default 6
+        """        
+        super().__init__()
+        folder = Path(folder)
+        self.data = get_pretraining_data(folder)
+        self.shuffle = shuffle
+        self.prefix = folder
+        self.image_size = image_size
+        self.n_param = n_param
+
+    def __len__(self):
+        return len(self.data)
+
+    def random_sample(self):
+        return self.__getitem__(randint(0, self.__len__() - 1))
+
+    def sequential_sample(self, ind):
+        if ind >= self.__len__() - 1:
+            return self.__getitem__(0)
+        return self.__getitem__(ind + 1)
+
+    def skip_sample(self, ind):
+        if self.shuffle:
+            return self.random_sample()
+        return self.sequential_sample(ind=ind)
+
+    def __getitem__(self, ind):
+        tokens = np.float32(np.array([0]).repeat(self.n_param)) #array([0,0]) for uncondional training
+        original_image = np.float32(self.data[ind])
+        arr = np.expand_dims(original_image,axis=0) # only one channel
+
+        return th.tensor(arr),th.tensor(np.float32(tokens))
+
+    
 
 class TextImageDataset(Dataset):
     def __init__(
